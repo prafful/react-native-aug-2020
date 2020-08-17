820d194425f09479b2e9a0da3c5f8eb5ca4c8696
prafful <prraful@gmail.com>
2020-08-17 03:09:58 -0700

    day 01-05 ScrollView

--- App.js
+++ App.js
@@ -1,53 +1,71 @@
 
 import React, { useState } from 'react';
-import { View, StyleSheet, Text, TextInput } from 'react-native';
+import { View, StyleSheet, Text, ScrollView, TouchableOpacity } from 'react-native';
 
 
 export default function App(){
 // [stateName, function-Who-Will-Change-This-State] = useState('intialValue')
- const [friendName, setFriendName]   = useState('Add Friend')
- const [friendDetail, setFriendDetail] = useState({
-                                                    location:'Chennai',
-                                                    years: 4                 
-                                                  })
-
-
-  const captureFriendLocation=(value)=>{
-    console.log(value);
-    setFriendDetail({
-                      location: value,
-                      years: friendDetail.years
-                    })
-  }
+ const [friends, setFriends]   = useState([
+                                            {
+                                              name:'OBB',
+                                              key:1,
+                                              location:'Chennai'
+                                            },
+                                            {
+                                              name:'CAS',
+                                              key:2,
+                                              location:'Bengaluru'
+                                            },
+                                            {
+                                              name:'OWIOH',
+                                              key:3,
+                                              location:'Japan'
+                                            },
+                                            {
+                                              name:'BNP',
+                                              key:4,
+                                              location:'Ahmedabad'
+                                            },
+                                            {
+                                              name:'OBB',
+                                              key:5,
+                                              location:'Chennai'
+                                            },
+                                            {
+                                            name:'CAS',
+                                            key:6,
+                                            location:'Bengaluru'
+                                          },
+                                          {
+                                            name:'OWIOH',
+                                            key:7,
+                                            location:'Japan'
+                                          },
+                                          {
+                                            name:'BNP',
+                                            key:8,
+                                            location:'Ahmedabad'
+                                          }
+                                      ])
+ 
 
   
   return (
     <View style={mystyles.maincontainer}>
-      <View style={mystyles.header}>
-        <Text style={mystyles.textformat}>{friendName}</Text>
-      </View>
-      <View style={mystyles.content}>
-        <TextInput 
-              style={mystyles.inputfriend} 
-              placeholder='friend location...'
-              onChangeText={captureFriendLocation}>
-        </TextInput>
-        <TextInput 
-              style={mystyles.inputfriend} 
-              keyboardType='numeric'
-              placeholder='friend since years...'
-              onChangeText={(value)=>{setFriendDetail({
-                                                        location:friendDetail.location,
-                                                        years: value
-                                                      })}}>
-        </TextInput>
-        <TextInput style={mystyles.inputfriend} placeholder='about friend...' multiline></TextInput>
-        <Text></Text>
-        <Text>Location: {friendDetail.location}</Text>
-        <Text>Years: {friendDetail.years}</Text>
-
+      <ScrollView>
+        {
+          friends.map(friend =>{
+            return (
+              <View key={friend.key}>
+                <TouchableOpacity onPress={()=>{console.log(friend.name)}}>
+                  <Text style={mystyles.listitem}>{friend.name}</Text>
+                </TouchableOpacity>
+              </View>
+            )
+          })
+        }
+      </ScrollView>    
         
-      </View>
     </View>
   )
 }
@@ -56,30 +74,13 @@ const mystyles = StyleSheet.create({
   maincontainer:{
     backgroundColor:'pink',
     flex:1,
-    alignItems:'center',
-    justifyContent:'center'
+    //alignItems:'center',
+    //justifyContent:'center'
   },
-  header:{
-    backgroundColor:'lightgrey',
+  listitem:{
+    marginTop:20,
+    fontSize:65,
+    backgroundColor:'goldenrod',
     padding:20,
-    width:240,
-    alignItems:'center',
-    justifyContent:'center'
-  },
-  textformat:{
-    color:'blue'
-    
-  },
-  content:{
-    backgroundColor:'coral',
-    padding:20
-  },
-  inputfriend:{
-    borderWidth:1,
-    borderColor:'pink',
-    padding:8,
-    margin:8,
-    width:184
   }
-
 })
