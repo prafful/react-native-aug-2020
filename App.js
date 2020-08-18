a96bfd4db50e07a66126a5e581eb884e04e9d140
prafful <prraful@gmail.com>
2020-08-18 02:36:19 -0700

    day02-03-axios-bug

--- App.js
+++ App.js
@@ -1,65 +1,47 @@
 
-import React, { useState } from 'react';
-import { View, StyleSheet, Text, TouchableOpacity, FlatList } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Text, TouchableOpacity, ScrollView } from 'react-native';
+import axios from 'axios'
+
 import Header from './components/header';
-import AddFriend from './components/addfriend';
+
 
 
 export default function App(){
 // [stateName, function-Who-Will-Change-This-State] = useState('intialValue')
- const [friends, setFriends]   = useState([
-                                            {
-                                              name:'OBB',
-                                              key:'1',
-                                              location:'Chennai',
-                                              myid: '1'
-                                            },
-                                            {
-                                              name:'CAS',
-                                              key:'2',
-                                              location:'Bengaluru',
-                                              myid: '2'
-                                            }
-                              
-                                      ])
- 
 
-  const listItemPressed = (id) =>{
-    console.log(id);
-    setFriends((prevFriends)=>{
-      console.log(prevFriends)
-      //One with myid == id will be removed from friends state
-      let newFriends = prevFriends.filter(friend => friend.myid != id)
-      return newFriends
-    })
-  }
+                               
+ const [users, setUsers] = useState([])
 
-  const addFriend =(name)=>{
-    console.log('addFriend function in App Component ' + name);
-    setFriends(prevFriends=>{
-      let tempKey = prevFriends.length + 1
-      return [{name, location:'Chennai', key:tempKey.toString(), myid:tempKey.toString()}, ...prevFriends]
-    })
-  }
+  useEffect(()=>{
+    axios.get('https://jsonplaceholder.typicode.com/users')
+          .then(res =>{
+            console.log(res.data)
+            setUsers(res.data)
+          })
+  })                                      
+
+
+  
 
   return (
     <View style={mystyles.maincontainer}>
-      <Header></Header>
-     <AddFriend addFriend={addFriend}></AddFriend>
-      <FlatList 
-          //numColumns={2}
-          keyExtractor={item => item.myid}
-          data={friends}
-          renderItem={(f)=>{
-            console.log('Value of friends:');
-            console.log(f);
+        <Header></Header>
+       
+        <ScrollView>
+        {
+          users.map(user =>{
             return (
-                    <TouchableOpacity onPress={()=>{listItemPressed(f.item.myid)}}>
-                      <Text style={mystyles.listitem}>{f.item.myid}.{f.item.name}</Text>
-                    </TouchableOpacity>
+              <View key={user.id}>
+                <TouchableOpacity onPress={()=>{console.log(user.name)}}>
+                  <Text style={mystyles.listitem}>{user.name}</Text>
+                </TouchableOpacity>
+              </View>
             )
-          }}
-     />
+          })
+        }
+      </ScrollView>
+ 
         
     </View>
   )
@@ -74,7 +56,7 @@ const mystyles = StyleSheet.create({
   },
   listitem:{
     marginTop:20,
-    fontSize:65,
+    fontSize:30,
     backgroundColor:'goldenrod',
     padding:20,
     color:'purple'
